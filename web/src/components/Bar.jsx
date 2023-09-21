c3e8b9206dfb608268662853f6adef2b938df267
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-21 14:42:18 +0500

    search icon added

--- web/src/components/Bar.jsx
+++ web/src/components/Bar.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Bar = () => {
   return (
     <div>
-      <div>
-        <span></span>
-        <span>Vector Search</span>
+      <div className=" flex items-center gap-4">
+        <span className=" text-[4rem] font-extrabold">VECTOR </span>
+        <span className="text-[4rem] font-extrabold">
+          <i class="bi bi-search"></i>
+        </span>
       </div>
     </div>
   );
